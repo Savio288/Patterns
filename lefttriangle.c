ae6321ab0c97c91a4c409850e215d7d8a4ce6c2b
Savio288 <113285187+Savio288@users.noreply.github.com>
2022-09-11 15:36:10 +0530

    Create lefttriangle.c

---
+++ lefttriangle.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   for (int i=0;i<5;i++)
+   {
+       int j=0;
+       while(j<=i)
+       {
+           printf("a");
+           j=j+1;
+       }
+       printf("\n");
+       
+   }
+   
+    return 0;
+}
